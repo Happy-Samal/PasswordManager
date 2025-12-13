9d783186e355ce9ff66a2e953c38e6f8906d3934
Happy Samal <rudrasamal007@gmail.com>
2025-12-13 22:31:02 +0530

    update models

--- backend/models/info.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const passOPSchema = new mongoose.Schema({
-    url:'string',
-    username:'string',
-    password:'string',
-});
-
-export const passOP = mongoose.model('passwords', passOPSchema);
