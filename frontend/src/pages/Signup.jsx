448ab92fe3f388b89081ed5d105946a1e8086f12
Happy Samal <rudrasamal007@gmail.com>
2025-12-13 23:00:27 +0530

    add console log

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -31,6 +31,7 @@ const Signup = () => {
   const handleSubmit = async(e) => {
     setLoading(true)
     e.preventDefault();
+    console.log("url" ,import.meta.env.VITE_BACKEND_URL);
     try{
       let response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/signup`,
         {
